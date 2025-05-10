20dc1511cac5ba8caf272134c9e8a7f693fecc0a
zubaer876 <84465298+zubaer876@users.noreply.github.com>
2025-05-10 11:28:14 +0600

    Add files via upload

---
+++ assets/revolution/php/youtube/class-youtube.php
@@ -0,0 +1,42 @@
+¿ÿÿÿÿ¿ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿüßÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ¿ÿÿÿÿïÿÿÿÿÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿ¿ÿÿÿÿÿÿÿûÿÿÿÿÿ÷ÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ÷ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿïÿÿßÿÿÿÿÿÿïÿÿÿÿÿÿýÿ¿ÿÿÿÿýÿÿÿÿïÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿý¿ÿÿÿ¿ÿÿÿÿ÷ÿÿÿÿÿÿÿÿïÿÿÿÿïÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ¿ÿÿÿÿÿ¿ÿÿÿÿÿÿ¿ÿÿÿÿÿïÿÿÿ÷ÿÿÿ÷ÿÿÿÿÿýÿÿÿÿÿÿÿÿÿ·ÿ÷ÿÿÿÿÿÿÿ÷ÿÿ¿ÿ¿ÿÿÿÿÿÿýÿÿÿÿÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿýÿÿÿÿýÿÿÿÿÿÿÿýÿÿÿÿÿÿÿïïÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ÷ÿÿÿÿ¿ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ÷ÿÿÿÿÿÿÿÿÿÿ¿ýÿÿÿÿÿÿÿï¿ÿÿÿÿÿID
+	 */
+	private $channel_id;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $api_key	Youtube API key.
+	 */
+	public function __construct($api_key,$channel_id) {
+		$this->api_key = $api_key;
+		$this->channel_id = $channel_id;
+	}
+
+
+	/**
+	 * Get Youtube Playlists
+	 *
+	 * @since    1.0.0
+	 */
+	public function get_playlists(){
+		//call the API and decode the response
+		$url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=".$this->channel_id."&key=".$this->api_key;
+		$rsp = json_decode(file_get_contents($url));
+		return $rsp->items;
+	}
+
+	/**
+	 * Get Youtube Playlist Items
+	 *
+	 * @since    1.0.0
+	 * @param    string    $playlist_id 	Youtube Playlist ID
+	 * @param    integer    $count 	Max videos count
+	 */
+	public function show_playlist_videos($playlist_id,$count=50){
+		//call the API and decode the response
+		$url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=".$playlist_id."&maxResults=".$count."&f¿ÿÿÿÿ¿ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿüßÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ¿ÿÿÿÿïÿÿÿÿÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿ¿ÿÿÿÿÿÿÿûÿÿÿÿÿ÷ÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ÷ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿïÿÿßÿÿÿÿÿÿïÿÿÿÿÿÿýÿ¿ÿÿÿÿýÿÿÿÿïÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿý¿ÿÿÿ¿ÿÿÿÿ÷ÿÿÿÿÿÿÿÿïÿÿÿÿïÿÿÿÿïÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ¿ÿÿÿÿÿ¿ÿÿÿÿÿÿ¿ÿÿÿÿÿïÿÿÿ÷ÿÿÿ÷ÿÿÿÿÿýÿÿÿÿÿÿÿÿÿ·ÿ÷ÿÿÿÿÿÿÿ÷ÿÿ¿ÿ¿ÿÿÿÿÿÿýÿÿÿÿÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿýÿÿÿÿýÿÿÿÿÿÿÿýÿÿÿÿÿÿÿïïÿÿÿýÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ÷ÿÿÿÿ¿ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ÷ÿÿÿÿÿÿÿÿÿÿ¿ýÿÿÿÿÿÿÿï¿ÿÿÿÿÿ(file_get_contents($url));
+		return $rsp->items;
+	}
+}
+?>
